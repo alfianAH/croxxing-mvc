6c23c40d7a6ace6280f378cc3289eb548d592a5d
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2023-08-14 17:12:13 +0800

    Add switch scene feature

--- Assets/Game/Scripts/Scene/MainMenu/MainMenuLauncher.cs
+++ Assets/Game/Scripts/Scene/MainMenu/MainMenuLauncher.cs
@@ -1,10 +1,27 @@
 using Croxxing.Boot;
 using Croxxing.Utility;
+using System.Collections;
 
 namespace Croxxing.Scene.MainMenu
 {
     public class MainMenuLauncher : SceneLauncher<MainMenuLauncher, MainMenuView>
     {
         public override string SceneName => GameScenes.MAIN_MENU;
+
+        protected override IEnumerator InitSceneObject()
+        {
+            _view.SetCallbacks(OnClickPlayButton, OnClickCreditsButton);
+            yield return null;
+        }
+
+        private void OnClickPlayButton()
+        {
+            SceneLoader.Instance.LoadScene(GameScenes.GAMEPLAY);
+        }
+
+        private void OnClickCreditsButton()
+        {
+            SceneLoader.Instance.LoadScene(GameScenes.CREDITS);
+        }
     }
 }
--- Assets/Game/Scripts/Scene/MainMenu/MainMenuView.cs
+++ Assets/Game/Scripts/Scene/MainMenu/MainMenuView.cs
@@ -1,9 +1,22 @@
 using Agate.MVC.Base;
+using UnityEngine.Events;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Croxxing.Scene.MainMenu
 {
     public class MainMenuView : BaseSceneView
     {
+        [SerializeField] private Button _playButton;
+        [SerializeField] private Button _creditsButton;
 
+        public void SetCallbacks(UnityAction onClickPlayButton, UnityAction onClickCreditsButton)
+        {
+            _playButton.onClick.RemoveAllListeners();
+            _playButton.onClick.AddListener(onClickPlayButton);
+            
+            _creditsButton.onClick.RemoveAllListeners();
+            _creditsButton.onClick.AddListener(onClickCreditsButton);
+        }
     }
 }
